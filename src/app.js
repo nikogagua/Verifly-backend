ad864be50bae0027f9a21a60b9ba229db41a335e
nikogagua <gaguanick2008@gmail.com>
2026-06-09 17:19:04 +0400

    adding dotenv to project setup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
 const express = require("express");
 const app = express();
 
@@ -5,6 +9,6 @@ app.get("/", (req, res) => {
   res.send("Verifly Backend is running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
