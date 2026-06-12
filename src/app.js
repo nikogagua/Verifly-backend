ffa7909afade434856f5b379d9e4576c6bfb9c82
nikogagua <gaguanick2008@gmail.com>
2026-06-12 22:35:54 +0400

    add User model and test MongoDB connection

--- src/app.js
+++ src/app.js
@@ -7,8 +7,27 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Verifly Backend is running 🚀");
+});
+
+//testing if user will be added to db by visiting /teest-user route
+const User = require("./models/user");
+app.get("/test-user", async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Test User",
+      email: "test@gmail.com",
+      password: "123456",
+      role: "owner",
+    });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 const startServer = async () => {
