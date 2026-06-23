35607c772bf20f242974d72c7a1deb3f68de349d
nikogagua <gaguanick2008@gmail.com>
2026-06-23 17:09:13 +0400

    implement store CRUD functionality

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -12,14 +12,115 @@ exports.createStore = async (req, res, next) => {
       });
     }
 
+    const {
+      name,
+      description,
+      category,
+      phone,
+      socials,
+      address,
+      logo,
+      primaryColor,
+      secondaryColor,
+      backgroundImage,
+    } = req.body;
     const store = await Store.create({
       owner: req.user.userId,
-      ...req.body,
+      name,
+      description,
+      category,
+      phone,
+      socials,
+      address,
+      logo,
+      primaryColor,
+      secondaryColor,
+      backgroundImage,
     });
 
     res.status(201).json({
       message: "Store created successfully",
       store,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.getStores = async (req, res, next) => {
+  try {
+    const stores = await Store.find();
+    res.status(200).json({
+      stores,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.myStore = async (req, res, next) => {
+  try {
+    const store = await Store.findOne({ owner: req.user.userId });
+    if (!store) {
+      return res.status(404).json({ message: "Store not found" });
+    }
+
+    res.status(200).json({ store });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.editStore = async (req, res, next) => {
+  try {
+    const store = await Store.findOne({ owner: req.user.userId });
+    if (!store) {
+      return res.status(404).json({ message: "Store not found" });
+    }
+
+    store.name = req.body.name || store.name;
+    store.description = req.body.description || store.description;
+    store.category = req.body.category || store.category;
+    store.phone = req.body.phone || store.phone;
+    store.socials = req.body.socials || store.socials;
+    store.address = req.body.address || store.address;
+    store.logo = req.body.logo || store.logo;
+    store.primaryColor = req.body.primaryColor || store.primaryColor;
+    store.secondaryColor = req.body.secondaryColor || store.secondaryColor;
+    store.backgroundImage = req.body.backgroundImage || store.backgroundImage;
+
+    await store.save();
+
+    res.status(200).json({
+      message: "Store updated successfully",
+      store,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+exports.deleteStore = async (req, res, next) => {
+  try {
+    const store = await Store.findOneAndDelete({
+      owner: req.user.userId,
+    });
+    if (!store) {
+      return res.status(404).json({
+        message: "Store not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Store deleted successfully",
     });
   } catch (err) {
     res.status(500).json({
--- src/routes/store.js
+++ src/routes/store.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const authMiddleware = require("../middleware/auth");
 const storeController = require("../controllers/storeController");
 
-// router.post("/", authMiddleware, storeController.createStore);
+router.get("/", storeController.getStores);
+
+router.post("/", authMiddleware, storeController.createStore);
+
+router.get("/me", authMiddleware, storeController.myStore);
+
+router.put("/me", authMiddleware, storeController.editStore);
+
+router.delete("/me", authMiddleware, storeController.deleteStore);
 
 module.exports = router;
